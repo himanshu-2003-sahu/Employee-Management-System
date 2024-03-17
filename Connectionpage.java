e82c7d647b66764ec4c1c0e92641c63e6426c47d
Himanshu sahu <160519921+himanshu-2003-sahu@users.noreply.github.com>
2024-03-18 00:21:54 +0530

    Create Connectionpage.java
    Connection page for mysql connected with system

---
+++ Connectionpage.java
@@ -0,0 +1,20 @@
+package employee.mnagement.system;
+import java.sql.*;
+public class Conn {
+    
+    Connection c;
+    Statement s;
+    public Conn(){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            
+            c=DriverManager.getConnection("jdbc:mysql:///employeemanagementsystem","root","Hima@2845");
+            s=c.createStatement();
+            
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    
+}
+    
+}
